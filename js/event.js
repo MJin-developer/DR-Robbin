f0a406fda6bfaea3c5838a3f486c8b1b3955a6ad
je1224 <rlawldms1385@gmail.com>
2020-08-25 11:05:01 +0900

    file 분리

---
+++ js/event.js
@@ -0,0 +1,19 @@
+function onload(){
+    var btn=document.getElementsByClassName('btn');
+    btn[0].focus();
+    var ev=document.getElementById('iframe');
+    ev.style.transition="opacity 1s";
+    ev.style.opacity="1.0";
+}
+function nowEvent(){
+    var ev=document.getElementById('iframe');
+    ev.src="./nowEvent.html";
+}
+function shopEvent(){
+    var ev=document.getElementById('iframe');
+    ev.src="./shopEvent.html";
+}
+function endEvent(){
+    var ev=document.getElementById('iframe');
+    ev.src="./endEvent.html";
+}
