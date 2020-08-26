b946be2332b7741bfd2726464f70a3c6902400af
Hankh <dlek26@naver.com>
2020-08-26 14:20:59 +0900

    zsczsczsc

---
+++ loadData.php
@@ -0,0 +1,19 @@
+<?php
+    header('Content-Type:text/plain; charset=utf-8');
+
+    $conn=mysqli_connect("localhost","hkh26","rudgns26!","hkh26");
+    mysqli_query($conn,"set names utf8");
+
+    $sql="SELECT * FROM drrobbine";
+    $result=mysqli_query($conn,$sql);
+    
+    $app_num=mysqli_num_rows($result);
+
+    for($i=0; $i<$app_num; $i++){
+        $app=mysqli_fetch_array($result,MYSQLI_ASSOC);
+        echo $app['no']."<br>";
+    }
+    // $person=json.encode($app);
+
+    // echo $person
+?>
