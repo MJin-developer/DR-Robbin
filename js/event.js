39b7620bdbde58b8ffe4db2910e5aa415acfcc4d
je1224 <rlawldms1385@gmail.com>
2020-08-27 11:44:46 +0900

    수정

--- js/event.js
+++ js/event.js
@@ -18,3 +18,13 @@ function endEvent(){
     var ev=document.getElementById('iframe');
     ev.src="./endEvent.html";
 }
+function ham(){
+    document.getElementById('myNav').style.display='flex';
+    document.getElementById('wrap').style.display='none';
+    document.getElementById('wrap2').style.display='flex';
+}
+function ham2(){
+    document.getElementById('myNav').style.display='none';
+    document.getElementById('wrap2').style.display='none';
+    document.getElementById('wrap').style.display='flex';
+}
